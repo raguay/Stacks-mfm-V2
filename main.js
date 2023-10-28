90f071e3784a7065c6f4115a9649804b212f9854
Richard Guay <raguay@customct.com>
2023-10-28 16:10:18 +0700

    Initial extension testing

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-var stacks = {
+const stacks = {
   extMan: null,
   fs: null,
   hdir: null,
   configdir: null,
   dir: "",
+  defaultStack: "",
   init: async function(extManager) {
     //
     // This function adds all the commands for working with stacks and 
@@ -13,15 +14,69 @@ var stacks = {
     stacks.fs = stacks.extMan.getLocalFS();
     stacks.hdir = await stacks.fs.getHomeDir();
     stacks.configdir = await stacks.extMan.getConfigDir();
-    stacks.dir = await stacks.fs.appendPaht(stacks.configdir, "Stacks");
+    stacks.dir = await stacks.fs.appendPath(stacks.configdir, "Stacks");
+    let dirExist = await stacks.fs.dirExists(stacks.dir);
+    if(!dirExist) {
+      // 
+      // the directory doesn't exist. Create it and the default stack.
+      //
+      await stacks.fs.createDir(stacks.dir);
+      stacks.defaultStack = await stacks.fs.appendPath(stacks.dir, "Default");
+      await stacks.fs.createDir(stacks.defaultStack);
+    } else {
+      // 
+      // The stacks directory exists, so create the path to the default one.
+      //
+      stacks.defaultStack = await stacks.fs.appendPath(stacks.dir, "Default");
+    }
     const addCommand = stacks.extMan.getCommands().addCommand;
-    addCommand('Get Stacks Directory', 'stacks.GetStackDir','Get the path to the stacks directory.', stacks.GetStackDir);
+    const addExtCommand = stacks.extMan.addExtCommand;
+    addExtCommand('Get Stacks Directory', 'Get the path to the stacks directory.', stacks.GetStackDir);
+    addExtCommand('Get Stacks List', 'Get a list of Stack directories.', stacks.GetStackNames)
+    addCommand('Copy to the Default Stack', 'stacks.CopyToDefaultStack', 'Copy the selection to the default stack.', stacks.CopyToDefaultStack);
+    addCommand('Move to the Default Stack', 'stacks.MoveToDefaultStack', 'Move the selection to the default stack.', stacks.MoveToDefaultStack);
+    addCommand('Paste from the Default Stack', 'stacks.PasteFromDefaultStack', 'Pastes the file in the default stack to the current directory.', stacks.PasteFromDefaultStack);
   },
   installKeyMaps: function() {
-    let newKeyboard = stacks.extMan.getExtCommand('addKeyboardShort').command;
   },
   GetStackDir: function() {
     return(stacks.dir);
+  },
+  GetStackNames: async function() {
+    // 
+    // This function reads the stacks directory for subfolders and returns a list of them.
+    //
+    return(await stacks.ext.getCommands().readDir(stacks.dir).map(item => item.name));
+  },
+  CopyToDefaultStack: async function() {
+    // 
+    // This function copies the current selection(s) to the default stack.
+    //
+    const copyEntries = stacks.extMan.getExtCommand("copyEntriesCommand");
+    const getSelection = stacks.extMan.getExtCommand("getSelectedFiles");
+    const files = await getSelection();
+    await copyEntries(files,stacks.defaultStack,true);
+  },
+  MoveToDefaultStack: async function() {
+    //
+    // This function moves the current selection to the default stack.
+    //
+    const moveEntries = stacks.extMan.getExtCommand("moveEntriesCommand");
+    const getSelection = stacks.extMan.getExtCommand("getSelectedFiles");
+    const files = await getSelection();
+    await moveEntries(files,stacks.defaultStack,true);
+  },
+  PasteFromDefaultStack: async function() {
+    // 
+    // This function pastes all the files and directories in the default stack to the
+    // current directory.
+    //
+    const copyEntries = stacks.extMan.getExtCommand("copyEntriesCommand");
+    const getCurrentFile = stacks.extMan.getExtCommand("getCurrentFile");
+    const files = await stacks.ext.getCommands().readDir(stacks.defaultStack);
+    const currentDirectory = getCurrentFile().dir;
+    copyEntries(files, currentDirectory);
   }
 };
 return (stacks);
+
